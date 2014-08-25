2d00e25d445ca95c2c14996aff2ae96918f8612a
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-08-25 22:28:58 +0000

    Creadas las clases y firma de los metodos

---
+++ src/main/codigo/Usuario.java
@@ -0,0 +1,44 @@
+package codigo;
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	private String nombreUsuario;
+	private String email;
+	private String fechaDeNacimiento;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+	public void setFechaDeNacimiento(String fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+	
+	
+
+}
---
+++ src/main/excepciones/UsuarioNoExiste.java
@@ -0,0 +1,5 @@
+package excepciones;
+
+public class UsuarioNoExiste extends Exception {
+
+}
