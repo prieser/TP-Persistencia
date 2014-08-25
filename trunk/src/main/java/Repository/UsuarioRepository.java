9f755ccf4c42ed934f8a27c6514b268b155f3200
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-08-25 22:52:15 +0000

    Se crear el Repository de Usuario

---
+++ trunk/src/main/java/Repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import codigo.Usuario;
+import excepciones.UsuarioYaExisteException;
+
+ public class UsuarioRepository {
+	 public static void RegistrarUsuario(Usuario usuario) throws UsuarioYaExisteException{
+		
+	 }
+	
+}
