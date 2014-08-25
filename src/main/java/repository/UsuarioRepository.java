7cc644ceb1a5f0dd485654bd09d2197aacca596c
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-08-25 23:47:26 +0000

    Hasta aqui. Continuar trabajando con el Repositorio de usuario.

--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -1,11 +1,30 @@
 package repository;
 
-import modelo.Usuario;
-import excepciones.UsuarioYaExisteException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
- public class UsuarioRepository {
-	 public static void RegistrarUsuario(Usuario usuario) throws UsuarioYaExisteException{
-	
+import modelo.Usuario;
+
+ public class UsuarioRepository extends Repository {
+	 
+	 public void RegistrarUsuario(Usuario usuario) throws Exception{
+		 //Connection connection = this.getConnection();
+		// this.ExisteUsuario(usuario);
+			 
+		 
+		 
 	 }
+	 
+	 //public boolean ExisteUsuario(Usuario usuario) throws Exception{
+	/*	 Connection conn = null;
+			PreparedStatement ps = null;
+			try{
+				ps = conn.prepareStatement("SELECT ID,NOMBRE,CODIGO FROM Aerolinea WHERE CODIGO = ?");
+				ps.setString(1, "UNA");
+				ResultSet rs = ps.executeQuery();
+			}
+		*/ 
+	 //}
 	
 }
