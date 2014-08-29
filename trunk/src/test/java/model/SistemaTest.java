2a130e87466e183e1d18abe18f3af84325fd809f
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-08-29 20:36:33 +0000

    [Colo] - Agregados algunos  tests para la clase Sistema.

--- trunk/src/test/java/model/SistemaTest.java
+++ trunk/src/test/java/model/SistemaTest.java
@@ -1,6 +1,13 @@
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import junit.framework.Assert;
 import modelo.Sistema;
 import modelo.Usuario;
 
@@ -12,33 +19,44 @@ import excepciones.UsuarioYaExisteException;
 
 public class SistemaTest {
 
-	// Necesito dos variables de la clase
+	// Necesito tres variables de clase.
 	private Usuario usuarioComunYCorriente;
 	private Sistema sistema;
+	private Connection connection;
+	private PreparedStatement ps;
 
 	@Before
 	public void setUp() throws Exception {
-		// Instancio el sistema y el usuario. Asigno estas instancias a las variables.
+		// Instancio el sistema y el usuario. Asigno estas instancias a las
+		// variables de clase. Por ultimo pido una conexion con la base de
+		// datos.
 		usuarioComunYCorriente = new Usuario("Juan", "Gomez", "juangomez", "jgomez@gmail.com", "01/08/2015");
 		sistema = new Sistema();
+		this.connection = this.getConnection();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (this.connection != null) {
+			this.connection.close(); // Cierro la conexion con la base de datos
+		}
+		if (this.ps != null) {
+			this.ps.close(); // Cierro el statement.
+		}
 	}
 
-	
 	@Test
-	public void testRegistrarUsuarioPorPrimeraVez() throws UsuarioYaExisteException {
+	public void testRegistrarUsuarioPorPrimeraVez() throws UsuarioYaExisteException, SQLException {
 		sistema.RegistrarUsuario(usuarioComunYCorriente);
-		// TODO: Falta agregar el assert que verifique si efecticamente se guardo el usuario en la db.
+		PreparedStatement ps = connection.prepareStatement("SELECT * FROM usuarios WHERE nombre = Juan AND apellido = Gomez AND nombreDeUsuario = juangomez AND email = jgomez@gmail.com");
+		ps.execute();
+		Assert.assertEquals("Se espera se haya encontrado al usuario insertado:", 1, ps.getUpdateCount());
 	}
 
-	@Test (expected = UsuarioYaExisteException.class)
+	@Test(expected = UsuarioYaExisteException.class)
 	public void testRegistrarUsuarioQueYaExisteEnLaBaseDeDatos() throws UsuarioYaExisteException {
 		sistema.RegistrarUsuario(usuarioComunYCorriente);
 		sistema.RegistrarUsuario(usuarioComunYCorriente);
-
 	}
 
 	@Test
@@ -54,6 +72,12 @@ public class SistemaTest {
 	@Test
 	public void testCambiarPassword() {
 		fail("Not yet implemented");
+	}
+
+	/* Metodo auxiliar para conexion con la base de datos */
+	private Connection getConnection() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		return DriverManager.getConnection("jdbc:mysql://localhost/database?user=root&password=root");
 	}
 
 }
