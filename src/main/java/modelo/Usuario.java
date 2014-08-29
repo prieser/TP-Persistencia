9f38967d793ea47d90fb85f43389faac41bf9d07
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-08-29 19:58:04 +0000

    Agregado el constructor con todos los parametros a la clase Usuario. Agregada la el testing para la clase Sistema.

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,44 +1,64 @@
 package modelo;
 
 public class Usuario {
-	
+
 	private String nombre;
 	private String apellido;
 	private String nombreUsuario;
 	private String email;
 	private String fechaDeNacimiento;
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getNombreUsuario() {
 		return nombreUsuario;
 	}
+
 	public void setNombreUsuario(String nombreUsuario) {
 		this.nombreUsuario = nombreUsuario;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getFechaDeNacimiento() {
 		return fechaDeNacimiento;
 	}
+
 	public void setFechaDeNacimiento(String fechaDeNacimiento) {
 		this.fechaDeNacimiento = fechaDeNacimiento;
 	}
-	
-	
+
+	public Usuario(String nombre, String apellido, String nombreUsuario, String email, String fechaDeNacimiento) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.nombreUsuario = nombreUsuario;
+		this.email = email;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+
+	public Usuario() {
+		super();
+	}
 
 }
