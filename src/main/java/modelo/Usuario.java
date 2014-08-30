a185774b63b274d1c89716f1ddcf7942c01ab18b
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-08-30 21:09:50 +0000

    [Colo] - Cambio de lugar el  testCambiarPassword() que estaba implementado en otro lado.

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -2,73 +2,73 @@ package modelo;
 
 public class Usuario {
 
-	private String nombre;
-	private String apellido;
-	private String nombreUsuario;
-	private String email;
-	private String fechaDeNacimiento;
-	private String contraseña;
+    private String nombre;
+    private String apellido;
+    private String nombreUsuario;
+    private String email;
+    private String fechaDeNacimiento;
+    private String contraseña;
 
-	public String getContraseña() {
-		return contraseña;
-	}
+    public String getContraseña() {
+        return contraseña;
+    }
 
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public String getApellido() {
+        return apellido;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
 
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getFechaDeNacimiento() {
-		return fechaDeNacimiento;
-	}
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
 
-	public void setFechaDeNacimiento(String fechaDeNacimiento) {
-		this.fechaDeNacimiento = fechaDeNacimiento;
-	}
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
 
-	public Usuario(String nombre, String apellido, String nombreUsuario, String email, String fechaDeNacimiento , String contraseña) {
-		super();
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.nombreUsuario = nombreUsuario;
-		this.email = email;
-		this.fechaDeNacimiento = fechaDeNacimiento;
-		this.contraseña = contraseña;
-	}
+    public Usuario(String nombre, String apellido, String nombreUsuario, String email, String fechaDeNacimiento, String contraseña) {
+        super();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.contraseña = contraseña;
+    }
 
-	public Usuario() {
-		super();
-	}
+    public Usuario() {
+        super();
+    }
 
 }
