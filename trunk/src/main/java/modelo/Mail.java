b16c6238126374b7aa83763476fdb93f248e1944
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-01 22:05:55 +0000

    [Zequi] - Arreglando la clase Mail

--- trunk/src/main/java/modelo/Mail.java
+++ trunk/src/main/java/modelo/Mail.java
@@ -1,13 +1,43 @@
 package modelo;
 
 public class Mail {
-	private	 String body;
-	private	 String subject;
-	private	 String to;
-	private	 String from;
-	
-	
-	
+	private String body;
+	private String subject;
+	private String to;
+	private String from;
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
 	public Mail(String body, String subject, String to, String from) {
 		super();
 		this.body = body;
@@ -15,6 +45,5 @@ public class Mail {
 		this.to = to;
 		this.from = from;
 	}
-	
-	
+
 }
