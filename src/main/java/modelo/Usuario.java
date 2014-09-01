6475bb83a8c6688111c6dc9f9e42d7ababe6ce4d
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-01 21:19:24 +0000

    Se refefine equals de usuario.

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -89,10 +89,32 @@ public class Usuario {
 	public boolean isActivo() {
 		return this.activo ;
 	}
-	
-	public boolean equals(Object obj){
-		Usuario usuario = (Usuario) obj;
-		return (usuario.getNombreUsuario() == this.getNombreUsuario());
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((nombreUsuario == null) ? 0 : nombreUsuario.hashCode());
+		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (nombreUsuario == null) {
+			if (other.nombreUsuario != null)
+				return false;
+		} else if (!nombreUsuario.equals(other.nombreUsuario))
+			return false;
+		return true;
+	}
+	
 
 }
