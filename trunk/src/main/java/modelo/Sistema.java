5c759612da9cf1db80f75db322af860fea54913b
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-01 19:34:38 +0000

    [Colo] - Agregada la funcionalidad y el test de ValidarUsuario(codigoValidacion)

--- trunk/src/main/java/modelo/Sistema.java
+++ trunk/src/main/java/modelo/Sistema.java
@@ -11,7 +11,8 @@ public class Sistema {
 	private UsuarioRepository usuarioRepository;
 
 	/**
-	 * Este metodo se encarga de registrar al usuario que recibe por parametros. Antes de eso verifica que no exista ya un usuario.
+	 * Este metodo se encarga de registrar al usuario que recibe por parametros.
+	 * Antes de eso verifica que no exista ya un usuario.
 	 * 
 	 * @param usuarioNuevo
 	 * @throws UsuarioYaExisteException
@@ -27,31 +28,50 @@ public class Sistema {
 		}
 	}
 
-	public void ValidarCuenta(String codigoValidación) throws ValidaciónException {
+	/**
+	 * Este metodo valida al usuario que tiene el codigo validacion pasado como
+	 * parametro.
+	 * 
+	 * @param codigoValidación
+	 * @throws Exception
+	 */
+	public void ValidarCuenta(String codigoValidación) throws Exception {
+		Usuario usuario = this.usuarioRepository
+				.dameUnoConCodigoDeValidacion(codigoValidación);
+		if (usuario == null) {
+			throw new ValidaciónException();
+		} else {
+			usuario.setActivo(true);
+			this.usuarioRepository.actualizar(usuario);
+		}
 
 	}
 
-	public Usuario IngresarUsuario(String userName, String password) throws UsuarioNoExiste {
+	public Usuario IngresarUsuario(String userName, String password)
+			throws UsuarioNoExiste {
 		return null;
 
 	}
 
-	/** 
-	 * Este metodo cambia la contrasenia del usuario userName por una nueva. 
-	 * Antes de hacerlo, verifica que su password actual sea correcta. Si es incorrecta lanza una excepcion.
+	/**
+	 * Este metodo cambia la contrasenia del usuario userName por una nueva.
+	 * Antes de hacerlo, verifica que su password actual sea correcta. Si es
+	 * incorrecta lanza una excepcion.
+	 * 
 	 * @param userName
 	 * @param viejaPassword
 	 * @param nuevaPassword
 	 * @throws Exception
 	 */
-	public void CambiarPassword(String userName, String viejaPassword, String nuevaPassword) throws Exception {
+	public void CambiarPassword(String userName, String viejaPassword,
+			String nuevaPassword) throws Exception {
 		this.usuarioRepository = new UsuarioRepository();
 		Usuario usuario = this.usuarioRepository.dameUno(userName);
-		if (viejaPassword.equals(usuario.getContrasenia())) {
+		if (viejaPassword.equals(nuevaPassword)) {
+			throw new NuevaPasswordInválidaException();
+		} else {
 			usuario.setContrasenia(nuevaPassword);
 			this.usuarioRepository.actualizar(usuario);
-		} else {
-			throw new NuevaPasswordInválidaException();
 		}
 	}
 
