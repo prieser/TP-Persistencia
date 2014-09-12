ca4b926bec727354ad4a4cec5d4742c75e0d5ec1
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-12 14:41:07 +0000

    [Colo] - Agregadas las clases que forman a la aerolinea

---
+++ src/main/java/modelo/aerolinea/Empresa.java
@@ -0,0 +1,11 @@
+package modelo.aerolinea;
+
+import java.util.Collection;
+
+/**
+ * Created by Pablo Rieser on 11/09/2014.
+ */
+public class Empresa {
+
+    private Collection<Aerolinea> aerolineas;
+}
