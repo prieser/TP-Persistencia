ce67a263a124c9fcebacc2a62eb9ad9fca53c49f
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-12 17:24:44 +0000

    zequi mas commits

--- src/main/java/modelo/aerolinea/Asiento.java
+++ src/main/java/modelo/aerolinea/Asiento.java
@@ -10,4 +10,39 @@ public class Asiento {
 	private boolean reservado;
 	private Usuario usuario;
 	private Categoria categoria;
+	private int numero;
+	
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public boolean estaReservado() {
+		return reservado;
+	}
+	public void setReservado(boolean reservado) {
+		this.reservado = reservado;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public void reservarAsiento (Usuario usuario){
+		this.setReservado(true);
+		this.setUsuario(usuario);
+		
+	}
+	
+	
 }
--- src/main/java/modelo/aerolinea/Empresa.java
+++ src/main/java/modelo/aerolinea/Empresa.java
@@ -2,10 +2,30 @@ package modelo.aerolinea;
 
 import java.util.Collection;
 
+import modelo.Usuario;
+
 /**
  * Created by Pablo Rieser on 11/09/2014.
  */
 public class Empresa {
 
     private Collection<Aerolinea> aerolineas;
-}
+    
+    
+    
+    public void reservarAsiento(Tramo tramo , Asiento asiento , Usuario usuario) {
+   
+/*    	recorrer la lista de asientos sacadas desde el tramo
+ * preguntar si el asiento esta ocupado si no esta ocupado reservarlo enviadonle el metodo al asiento
+ */
+         for (Asiento butaca : tramo.getAsientos()) {
+    		if (asiento.getNumero() == butaca.getNumero())
+    		{
+    			asiento.reservarAsiento(usuario);
+    			}
+    		}
+    		
+        }
+    	
+    }
+    
