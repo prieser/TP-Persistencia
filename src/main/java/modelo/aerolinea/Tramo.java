638b38f6d44e78f795af2af79b0dcfebe772126f
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-12 15:08:51 +0000

    Zequi: Cambios

--- src/main/java/modelo/aerolinea/Tramo.java
+++ src/main/java/modelo/aerolinea/Tramo.java
@@ -1,7 +1,15 @@
 package modelo.aerolinea;
 
+import java.util.Collection;
+
 /**
  * Created by Pablo Rieser on 11/09/2014.
  */
 public class Tramo {
+	private Collection<Asiento> asientos;
+	private String origen;
+	private String destino;
+	private String horaDeLlegada;
+	private String horaDeSalida;
+	private int precio;
 }
--- src/main/java/modelo/aerolinea/Vuelo.java
+++ src/main/java/modelo/aerolinea/Vuelo.java
@@ -1,7 +1,10 @@
 package modelo.aerolinea;
 
+import java.util.Collection;
+
 /**
  * Created by Pablo Rieser on 11/09/2014.
  */
 public class Vuelo {
+	private Collection<Tramo> tramos;
 }
