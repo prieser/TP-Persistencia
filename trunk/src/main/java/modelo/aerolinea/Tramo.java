635a1ef70c23c32396e8a926b9791dad285508fb
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-12 17:24:44 +0000

    zequi mas commits

--- trunk/src/main/java/modelo/aerolinea/Tramo.java
+++ trunk/src/main/java/modelo/aerolinea/Tramo.java
@@ -12,4 +12,43 @@ public class Tramo {
 	private String horaDeLlegada;
 	private String horaDeSalida;
 	private int precio;
+	
+	
+	
+	public Collection<Asiento> getAsientos() {
+		return asientos;
+	}
+	public void setAsientos(Collection<Asiento> asientos) {
+		this.asientos = asientos;
+	}
+	public String getOrigen() {
+		return origen;
+	}
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public String getHoraDeLlegada() {
+		return horaDeLlegada;
+	}
+	public void setHoraDeLlegada(String horaDeLlegada) {
+		this.horaDeLlegada = horaDeLlegada;
+	}
+	public String getHoraDeSalida() {
+		return horaDeSalida;
+	}
+	public void setHoraDeSalida(String horaDeSalida) {
+		this.horaDeSalida = horaDeSalida;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
 }
