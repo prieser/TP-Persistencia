9c271b592d795b05f20812012522441b68ec7fa1
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-15 17:54:34 +0000

    Se agrega logica

--- src/main/java/modelo/aerolinea/Aerolinea.java
+++ src/main/java/modelo/aerolinea/Aerolinea.java
@@ -2,10 +2,16 @@ package modelo.aerolinea;
 
 import java.util.Collection;
 
+
 /**
  * Created by Pablo Rieser on 11/09/2014.
  */
 public class Aerolinea {
 
     private Collection<Vuelo> vuelos;
+    
+    public Collection<Vuelo> vuelosOfertados(){
+    	return this.vuelos ;
+    	
+    }
 }
