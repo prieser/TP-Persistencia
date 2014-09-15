36495eeb6af0917e20f23547d43cd55410eeae10
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-15 21:17:20 +0000

    Puto el q lee

--- trunk/src/main/java/modelo/aerolinea/Aerolinea.java
+++ trunk/src/main/java/modelo/aerolinea/Aerolinea.java
@@ -8,9 +8,36 @@ import java.util.Collection;
  */
 public class Aerolinea {
 
-    private Collection<Vuelo> vuelos;
+    private int id;
+	private Collection<Vuelo> vuelos;
+    private String nombre;
+    private String direccion;
     
-    public Collection<Vuelo> vuelosOfertados(){
+    public Collection<Vuelo> getVuelos() {
+		return vuelos;
+	}
+
+	public void setVuelos(Collection<Vuelo> vuelos) {
+		this.vuelos = vuelos;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Collection<Vuelo> vuelosOfertados(){
     	return this.vuelos ;
     	
     }
