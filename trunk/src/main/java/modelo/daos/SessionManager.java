fd1208380d4b6c4f5a84bc59b875752ef20ba485
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-15 21:36:53 +0000

    Mas cambios

---
+++ trunk/src/main/java/modelo/daos/SessionManager.java
@@ -0,0 +1,61 @@
+package modelo.daos;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+/* import org.hibernate.classic.Session;*/
+
+
+import modelo.servicios.Operation;
+
+public class SessionManager {
+	
+	private static SessionFactory sessionFactory;
+	private static ThreadLocal<Session> tlSession = new ThreadLocal<Session>();
+	
+	public synchronized static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration cfg = new Configuration();
+			cfg.configure();
+
+			sessionFactory = cfg.buildSessionFactory();
+		}
+
+		return sessionFactory;
+	}
+	
+	public static <T> T runInSession(Operation<T> cmd){
+		SessionFactory sessionFactory = SessionManager.getSessionFactory();
+		Transaction transaction = null;
+		T result = null;
+		Session session = null;
+		
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+
+			tlSession.set(session);
+			
+			result = cmd.execute();
+
+			session.flush();
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new RuntimeException(e);
+		} finally {
+			if (session != null)
+				session.close();
+			tlSession.set(null);
+		}
+		
+		return result;
+	}
+
+	public static Session getSession() {
+		return tlSession.get();
+	}
+
+}
