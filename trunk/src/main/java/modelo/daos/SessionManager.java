ac83a54948e52e5004655175d4790fe63c500856
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-19 17:19:21 +0000

    [Colo] - Intento de mappear Empresa a la base de datos

--- trunk/src/main/java/modelo/daos/SessionManager.java
+++ trunk/src/main/java/modelo/daos/SessionManager.java
@@ -1,61 +1,59 @@
 package modelo.daos;
 
+import modelo.servicios.Operation;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-/* import org.hibernate.classic.Session;*/
 
-
-import modelo.servicios.Operation;
 
 public class SessionManager {
-	
-	private static SessionFactory sessionFactory;
-	private static ThreadLocal<Session> tlSession = new ThreadLocal<Session>();
-	
-	public synchronized static SessionFactory getSessionFactory() {
-		if (sessionFactory == null) {
-			Configuration cfg = new Configuration();
-			cfg.configure();
 
-			sessionFactory = cfg.buildSessionFactory();
-		}
+    private static SessionFactory sessionFactory;
+    private static ThreadLocal<Session> tlSession = new ThreadLocal<Session>();
 
-		return sessionFactory;
-	}
-	
-	public static <T> T runInSession(Operation<T> cmd){
-		SessionFactory sessionFactory = SessionManager.getSessionFactory();
-		Transaction transaction = null;
-		T result = null;
-		Session session = null;
-		
-		try {
-			session = sessionFactory.openSession();
-			transaction = session.beginTransaction();
+    public synchronized static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration cfg = new Configuration();
+            cfg.configure();
 
-			tlSession.set(session);
-			
-			result = cmd.execute();
+            sessionFactory = cfg.buildSessionFactory();
+        }
 
-			session.flush();
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null)
-				transaction.rollback();
-			throw new RuntimeException(e);
-		} finally {
-			if (session != null)
-				session.close();
-			tlSession.set(null);
-		}
-		
-		return result;
-	}
+        return sessionFactory;
+    }
 
-	public static Session getSession() {
-		return tlSession.get();
-	}
+    public static <T> T runInSession(Operation<T> cmd){
+        SessionFactory sessionFactory = SessionManager.getSessionFactory();
+        Transaction transaction = null;
+        T result = null;
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            tlSession.set(session);
+
+            result = cmd.execute();
+
+            session.flush();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            if (session != null)
+                session.close();
+            tlSession.set(null);
+        }
+
+        return result;
+    }
+
+    public static Session getSession() {
+        return tlSession.get();
+    }
 
 }
