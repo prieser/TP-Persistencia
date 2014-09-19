f5562190c05b9209271c0a00bf43c79d8ee6f151
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-19 15:58:30 +0000

    [Colo] - Primer intento de mapeo a la db de la entidad Empresa

--- src/main/java/modelo/aerolinea/Empresa.java
+++ src/main/java/modelo/aerolinea/Empresa.java
@@ -9,23 +9,36 @@ import modelo.usuario.Usuario;
  */
 public class Empresa {
 
+    private Integer idEmpresa;
     private Collection<Aerolinea> aerolineas;
-    
-    
-    
-    public void reservarAsiento(Tramo tramo , Asiento asiento , Usuario usuario ) {
-   
-/*    	recorrer la lista de asientos sacadas desde el tramo
- * preguntar si el asiento esta ocupado si no esta ocupado reservarlo enviadonle el metodo al asiento
- */
-         for (Asiento butaca : tramo.getAsientos()) {
-    		if (asiento.getNumero() == butaca.getNumero())
-    		{
-    			asiento.reservarAsiento(usuario);
-    			}
-    		}
-    		
-        }
-    	
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
     }
+
+    public void setIdEmpresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public Collection<Aerolinea> getAerolineas() {
+        return aerolineas;
+    }
+
+    public void setAerolineas(Collection<Aerolinea> aerolineas) {
+        this.aerolineas = aerolineas;
+    }
+
+    public void reservarAsiento(Tramo tramo, Asiento asiento, Usuario usuario) {
+   
+        /* Recorrer la lista de asientos sacadas desde el tramo preguntar si el asiento esta ocupado
+         si no esta ocupado reservarlo enviadonle el metodo al asiento */
+        for (Asiento butaca : tramo.getAsientos()) {
+            if (asiento.getNumero() == butaca.getNumero()) {
+                asiento.reservarAsiento(usuario);
+            }
+        }
+
+    }
+
+}
     
