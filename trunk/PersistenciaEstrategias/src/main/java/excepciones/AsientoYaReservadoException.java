4655a6d48b322b6d1761baf02cb03b58805bbebf
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-09-25 23:26:28 +0000

    Borre la carpeta src que quedo en la raiz

--- trunk/PersistenciaEstrategias/src/main/java/excepciones/AsientoYaReservadoException.java
+++
@@ -1,6 +0,0 @@
-package excepciones;
-
-public class AsientoYaReservadoException extends Throwable {
-    public AsientoYaReservadoException(String s) {
-    }
-}
--- trunk/PersistenciaEstrategias/src/main/java/modelo/Turista.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-/**
- * Created by Pablo Rieser on 11/09/2014.
- */
-public class Turista extends Categoria {
-
-
-}
