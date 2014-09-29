ba1a771352ed052f6b895cd197c50b9ce4e98db7
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-09-29 18:20:58 +0000

    Se agregan los test para el requerimiento del tramo.

--- PersistenciaEstrategias/src/main/java/modelo/managers/AsientoManager.java
+++ PersistenciaEstrategias/src/main/java/modelo/managers/AsientoManager.java
@@ -1,5 +1,14 @@
 package modelo.managers;
 
+import modelo.aerolinea.Asiento;
+import modelo.daos.AsientoDAO;
+
+
 public class AsientoManager {
+	
+	public Object guardarAsiento(Asiento asiento) {
+		new AsientoDAO().save(asiento);
+		return null;
+	}
 
 }
