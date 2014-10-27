bc94bf9cb73ba1adb3f51371b55c83d696b02c24
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-10-27 15:08:36 +0000

    arreglado una linea al pedo

--- trunk/PersistenciaEstrategias/src/main/java/modelo/busquedas/Busqueda.java
+++ trunk/PersistenciaEstrategias/src/main/java/modelo/busquedas/Busqueda.java
@@ -3,28 +3,25 @@ package modelo.busquedas;
 import java.util.ArrayList;
 
 public class Busqueda {
-	
-	private ArrayList<Criterio> criterios;
-	private Orden orden;
-	
-	public void agregarCriterio(Criterio criterioAAgregar) {
-		this.criterios.add(criterioAAgregar);
-	}
 
-	public String getQuery() {
-		String query = "from Vuelo WHERE";
+    private ArrayList<Criterio> criterios;
+    private Orden orden;
 
-		for (Criterio unCriterio : this.criterios) {
-			query+= unCriterio.getQuery() ;
+    public void agregarCriterio(Criterio criterioAAgregar) {
+        this.criterios.add(criterioAAgregar);
+    }
+
+    public String getQuery() {
+        String query = "from Vuelo WHERE";
+
+        for (Criterio unCriterio : this.criterios) {
             query = query + unCriterio.getQuery();
 
-		}
-
-			query+= this.orden.getQuery();
-	
-		return query;
+        }
+        query += this.orden.getQuery();
+        return query;
 
 
-	}
+    }
 }
-	
+
