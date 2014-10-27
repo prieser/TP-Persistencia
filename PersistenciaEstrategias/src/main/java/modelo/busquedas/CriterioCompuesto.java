9891e20a9ffabd0767ae933b8800980bdee267a0
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-10-27 14:03:22 +0000

    Operadores AND y OR

--- PersistenciaEstrategias/src/main/java/modelo/busquedas/CriterioCompuesto.java
+++ PersistenciaEstrategias/src/main/java/modelo/busquedas/CriterioCompuesto.java
@@ -8,6 +8,14 @@ public class CriterioCompuesto extends Criterio {
 	private Criterio criterio2;
 	private Operador operador;
 
+
+    public CriterioCompuesto(Criterio criterioSimple1, Criterio criterioSimple2, Operador operador){
+        super();
+        this.criterio1 = criterioSimple1;
+        this.criterio2 = criterioSimple2;
+        this.operador = operador;
+    }
+
 	@Override
 	public String getQuery() {
 		return "(" + criterio1.getQuery() + operador.getOperador() + criterio2.getQuery() + ")";
