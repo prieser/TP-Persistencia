04860ad6c167147b856cec910596f0e15586e002
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-10-27 15:13:06 +0000

    Agregados los getters y setters

--- trunk/PersistenciaEstrategias/src/main/java/modelo/busquedas/CriterioCompuesto.java
+++ trunk/PersistenciaEstrategias/src/main/java/modelo/busquedas/CriterioCompuesto.java
@@ -3,22 +3,45 @@ package modelo.busquedas;
 import modelo.busquedas.operadores.Operador;
 
 public class CriterioCompuesto extends Criterio {
-	
-	private Criterio criterio1;
-	private Criterio criterio2;
-	private Operador operador;
 
+    private Criterio criterio1;
+    private Criterio criterio2;
+    private Operador operador;
 
-    public CriterioCompuesto(Criterio criterioSimple1, Criterio criterioSimple2, Operador operador){
+    public Criterio getCriterio1() {
+        return criterio1;
+    }
+
+    public void setCriterio1(Criterio criterio1) {
+        this.criterio1 = criterio1;
+    }
+
+    public Criterio getCriterio2() {
+        return criterio2;
+    }
+
+    public void setCriterio2(Criterio criterio2) {
+        this.criterio2 = criterio2;
+    }
+
+    public Operador getOperador() {
+        return operador;
+    }
+
+    public void setOperador(Operador operador) {
+        this.operador = operador;
+    }
+
+    public CriterioCompuesto(Criterio criterioSimple1, Criterio criterioSimple2, Operador operador) {
         super();
         this.criterio1 = criterioSimple1;
         this.criterio2 = criterioSimple2;
         this.operador = operador;
     }
 
-	@Override
-	public String getQuery() {
-		return "(" + criterio1.getQuery() + operador.getOperador() + criterio2.getQuery() + ")";
-	}
+    @Override
+    public String getQuery() {
+        return "(" + criterio1.getQuery() + operador.getOperador() + criterio2.getQuery() + ")";
+    }
 
 }
