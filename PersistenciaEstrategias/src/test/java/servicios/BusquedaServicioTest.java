e14b69c2d05d7455d4c1a6590fd901710c294d2c
sergio.edgardo.delgadillo <sergio.edgardo.delgadillo@gmail.com>
2014-11-07 19:22:27 +0000

    Tratando de terminar el test de BuscarVuelos

--- PersistenciaEstrategias/src/test/java/servicios/BusquedaServicioTest.java
+++ PersistenciaEstrategias/src/test/java/servicios/BusquedaServicioTest.java
@@ -1,7 +1,5 @@
 package servicios;
 
-import modelo.aerolinea.Aerolinea;
-import modelo.aerolinea.Asiento;
 import modelo.aerolinea.Tramo;
 import modelo.aerolinea.Vuelo;
 import modelo.busquedas.Busqueda;
@@ -9,7 +7,6 @@ import modelo.busquedas.Criterio;
 import modelo.busquedas.Orden;
 import modelo.busquedas.operadores.And;
 import modelo.busquedas.operadores.Or;
-import modelo.managers.AerolineaManager;
 import modelo.managers.BusquedaManager;
 import modelo.managers.TramoManager;
 import modelo.managers.VueloManager;
@@ -19,13 +16,19 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class BusquedaServicioTest extends  AbstractHibernateTest {
+public class BusquedaServicioTest extends AbstractHibernateTest {
 
     Usuario usuarioQueRealizaBusqueda;
     Busqueda busquedaARealizar;
     VueloManager vueloManager;
     TramoManager tramoManager;
     BusquedaManager busquedaManager;
+    Vuelo v1 = new Vuelo();
+    Vuelo v2 = new Vuelo();
+    Tramo t1 = new Tramo("CODT1", "BsAs", "Montevideo", new Date(20141105), new Date(20141105), 300);
+    Tramo t2 = new Tramo("CODT2", "BsAs", "Santiago de Chile", new Date(20141109), new Date(201411010), 500);
+    Tramo t3 = new Tramo("CODT3", "BsAs", "Santiago de Chile", new Date(20141110), new Date(201411010), 500);
+    Tramo t4 = new Tramo("CODT4", "BsAs", "San Pablo", new Date(20141111), new Date(201411011), 500);
 
     @Override
     public void setUp() throws Exception {
@@ -34,6 +37,14 @@ public class BusquedaServicioTest extends  AbstractHibernateTest {
         this.busquedaManager = new BusquedaManager();
         this.tramoManager = new TramoManager();
         this.vueloManager = new VueloManager();
+
+        v1.agregarTramo(t1);
+        v1.agregarTramo(t2);
+        v2.agregarTramo(t3);
+        v2.agregarTramo(t4);
+
+        vueloManager.guardarVuelo(v1);
+        vueloManager.guardarVuelo(v2);
     }
 
     @Test
@@ -49,26 +60,10 @@ public class BusquedaServicioTest extends  AbstractHibernateTest {
     }
 
     @Test
-    public void testBuscarVuelos() {
-
-        Vuelo v1 = new Vuelo();
-        Vuelo v2 = new Vuelo();
-
-        Tramo t1 = new Tramo("CODT1", "BsAs", "Montevideo", new Date(20141105), new Date(20141105), 300);
-        Tramo t2 = new Tramo("CODT2", "BsAs", "Santiago de Chile", new Date(20141109), new Date(201411010), 500);
-        Tramo t3 = new Tramo("CODT3", "BsAs", "Santiago de Chile", new Date(20141110), new Date(201411010), 500);
-        Tramo t4 = new Tramo("CODT4", "BsAs", "San Pablo", new Date(20141111), new Date(201411011), 500);
-        v1.agregarTramo(t1);
-        v1.agregarTramo(t2);
-        v2.agregarTramo(t3);
-        v2.agregarTramo(t4);
-
-        vueloManager.guardarVuelo(v1);
-        vueloManager.guardarVuelo(v2);
-
+    public void testBuscarVuelosConUnCriterio() {
         Criterio c1 = new Criterio("ORIGEN", "BsAs");
-//        Criterio c2 = new Criterio("DESTINO", "Santiago de Chile");
-       busquedaARealizar.agregarCriterio(c1);
-       ArrayList<Vuelo> resultado =  new BusquedaManager().buscarVuelos(busquedaARealizar);
+        busquedaARealizar.agregarCriterio(c1);
+        ArrayList<Vuelo> resultado = new BusquedaManager().buscarVuelos(busquedaARealizar);
+
     }
 }
