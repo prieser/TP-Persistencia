b13b7615137c49a30f05610aa8ea4800c72b80ed
sergio.edgardo.delgadillo@gmail.com <sergio.edgardo.delgadillo@gmail.com@9f8d27d0-050a-8cb0-7d1c-f6e672cb7ff5>
2014-11-10 22:59:17 +0000

    Arreglado el mapeo de Orden

--- trunk/PersistenciaEstrategias/src/main/java/modelo/busquedas/orden/Orden.java
+++ trunk/PersistenciaEstrategias/src/main/java/modelo/busquedas/orden/Orden.java
@@ -3,7 +3,6 @@ package modelo.busquedas.orden;
 public abstract class Orden {
 
     private int idOrden;
-    private String valor;
 
     public int getIdOrden() {
         return idOrden;
@@ -11,14 +10,6 @@ public abstract class Orden {
 
     public void setIdOrden(int idOrden) {
         this.idOrden = idOrden;
-    }
-
-    public String getValor() {
-        return valor;
-    }
-
-    public void setValor(String valor) {
-        this.valor = valor;
     }
 
     public abstract String getQuery();
